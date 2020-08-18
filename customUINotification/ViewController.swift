c1451f2b936e75915f2cad737f5bfb9d46881d9a
jawaheralaggl <jawaheralaggl@gmail.com>
2020-08-18 12:29:59 +0300

    Initial Commit

---
+++ customUINotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customUINotification
+//
+//  Created by Jawaher Alagel on 8/18/20.
+//  Copyright © 2020 Jawaher Alaggl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
